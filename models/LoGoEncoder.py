9a8bddb0a83c2c7a92fc97832c56b432aed64dc7
KhangKagTa19 <khang.ta1902@hcmut.edu.vn>
2025-03-15 11:11:59 +0700

    fix encoder 512 & add DyT

--- models/LoGoEncoder.py
+++ models/LoGoEncoder.py
@@ -28,6 +28,17 @@ class qkv_transform(nn.Conv1d):
 def conv1x1(in_planes, out_planes, stride=1):
         """1x1 convolution"""
         return nn.Conv2d(in_planes, out_planes, kernel_size=1, stride=stride, bias=False)
+
+class DyT(nn.Module):
+    def __init__(self, num_features, alpha_init_value=0.5):
+        super().__init__()
+        self.alpha = nn.Parameter(torch.ones(1) * alpha_init_value)
+        self.weight = nn.Parameter(torch.ones(num_features))
+        self.bias = nn.Parameter(torch.zeros(num_features))
+    
+    def forward(self, x):
+        x = torch.tanh(self.alpha * x)
+        return x * self.weight + self.bias
     
 class AxialAttention_gated_data(nn.Module):
     def __init__(self, in_planes, out_planes, groups=8, kernel_size=64,
@@ -46,12 +57,14 @@ class AxialAttention_gated_data(nn.Module):
         # Multi-head self attention
         self.qkv_transform = qkv_transform(in_planes, out_planes * 2, kernel_size=1, stride=1,
                                            padding=0, bias=False)
+        
         self.bn_qkv = nn.BatchNorm1d(out_planes * 2)
         self.bn_similarity = nn.BatchNorm2d(groups * 3)
+        self.bn_output = nn.BatchNorm1d(out_planes * 2)
         #self.bn_qk = nn.BatchNorm2d(groups)
         #self.bn_qr = nn.BatchNorm2d(groups)
         #self.bn_kr = nn.BatchNorm2d(groups)
-        self.bn_output = nn.BatchNorm1d(out_planes * 2)
+        
 
         self.fcn1 = nn.Linear(in_planes, in_planes)
         self.fcn2 = nn.Linear(in_planes, 4)
@@ -65,8 +78,9 @@ class AxialAttention_gated_data(nn.Module):
         self.register_buffer('flatten_index', relative_index.view(-1))
         if stride > 1:
             self.pooling = nn.AvgPool2d(stride, stride=stride)
-
+        self.dyt= DyT()
         self.reset_parameters()
+        
         # self.print_para()
 
     def forward(self, x):
@@ -94,7 +108,8 @@ class AxialAttention_gated_data(nn.Module):
         # Transformations
         # import pdb
         # pdb.set_trace()
-        qkv = self.bn_qkv(self.qkv_transform(x))
+        # qkv = self.bn_qkv(self.qkv_transform(x))
+        qkv = self.dyt(self.qkv_transform(x))
         q, k, v = torch.split(qkv.reshape(N * W, self.groups, self.group_planes * 2, H), [self.group_planes // 2, self.group_planes // 2, self.group_planes], dim=2)
 
         # Calculate position embedding
@@ -116,7 +131,8 @@ class AxialAttention_gated_data(nn.Module):
         # kr = torch.mul(kr, torch.sigmoid(self.f_kr))
 
         stacked_similarity = torch.cat([qk, qr, kr], dim=1)
-        stacked_similarity = self.bn_similarity(stacked_similarity).view(N * W, 3, self.groups, H, H).sum(dim=1)
+        stacked_similarity = self.dyt(stacked_similarity).view(N * W, 3, self.groups, H, H).sum(dim=1)
+        #stacked_similarity = self.bn_similarity(stacked_similarity).view(N * W, 3, self.groups, H, H).sum(dim=1)
         #stacked_similarity = self.bn_qr(qr) + self.bn_kr(kr) + self.bn_qk(qk)
         # (N, groups, H, H, W)
         similarity = F.softmax(stacked_similarity, dim=3)
@@ -130,8 +146,8 @@ class AxialAttention_gated_data(nn.Module):
         sve = sig4.reshape(-1, 1, 1, 1).contiguous()*sve
 
         stacked_output = torch.cat([sv, sve], dim=-1).view(N * W, self.out_planes * 2, H)
-        output = self.bn_output(stacked_output).view(N, W, self.out_planes, 2, H).sum(dim=-2)
-
+        #output = self.bn_output(stacked_output).view(N, W, self.out_planes, 2, H).sum(dim=-2)
+        output = self.dyt(stacked_output).view(N, W, self.out_planes, 2, H).sum(dim=-2)
         if self.width:
             output = output.permute(0, 2, 1, 3)
         else:
@@ -168,12 +184,14 @@ class AxialBlock_gated_data(nn.Module):
         self.relu = nn.ReLU(inplace=False)  # Changed from True to False
         self.downsample = downsample
         self.stride = stride
+        self.dyt = DyT()
 
     def forward(self, x):
         identity = x
 
         out = self.conv_down(x)
-        out = self.bn1(out)
+        #out = self.bn1(out)
+        out = self.dyt(out)
         out = self.relu(out)
 
         # 2 layers: hight and width
@@ -182,7 +200,8 @@ class AxialBlock_gated_data(nn.Module):
         out = self.relu(out)
 
         out = self.conv_up(out)
-        out = self.bn2(out)
+        #out = self.bn2(out)
+        out = self.dyt(out)
         out = self.relu(out)  # Changed from in-place to not in-place
 
         return out
@@ -240,48 +259,48 @@ class LoGoEncoder(nn.Module):
         self.norm_layer = nn.BatchNorm2d
         self.conv1 = nn.Sequential(
             # Layer 1: 256x256 -> 128x128
-            nn.Conv2d(3, 1024, kernel_size=3, stride=2, padding=1),
+            nn.Conv2d(3, 512, kernel_size=3, stride=2, padding=1),
             #nn.BatchNorm2d(256),
             nn.ReLU(inplace=False), 
             
             # Layer 2: 128x128 -> 64x64
-            nn.Conv2d(1024, 1024, kernel_size=3, stride=2, padding=1, bias = True),
+            nn.Conv2d(512, 512, kernel_size=3, stride=2, padding=1, bias = True),
             #nn.BatchNorm2d(512),
             nn.ReLU(inplace=False), 
         )
         
-        self.bn1 = self.norm_layer(1024)
+        self.bn1 = self.norm_layer(512)
         self.conv1_p = nn.Sequential(
-            nn.Conv2d(3, 1024, kernel_size=3, stride=2, padding=1),
+            nn.Conv2d(3, 512, kernel_size=3, stride=2, padding=1),
             #nn.BatchNorm2d(256),
             nn.ReLU(inplace=False), 
             
             # Layer 2: 128x128 -> 64x64
-            nn.Conv2d(1024, 1024, kernel_size=3, stride=2, padding=1, bias = True),
+            nn.Conv2d(512, 512, kernel_size=3, stride=2, padding=1, bias = True),
             #nn.BatchNorm2d(512),
             nn.ReLU(inplace=False), 
         )
-        self.bn1_p = self.norm_layer(1024)
+        self.bn1_p = self.norm_layer(512)
         self.relu = nn.ReLU(inplace=False)
         
         self.layer = AxialBlock_gated_data(
-                        inplanes=1024,     
-                        planes=1024,   
+                        inplanes=512,     
+                        planes=512,   
                         kernel_size=64
                     )
         self.layer_p = AxialBlock_gated_data(
-                        inplanes=1024,     
-                        planes=1024,   
+                        inplanes=512,     
+                        planes=512,   
                         kernel_size=16
                     )
         self.adjust_p = nn.Sequential(
-            nn.Conv2d(1024, 1024, kernel_size=1, stride=1, padding=0),
-            CBAM(channels=1024)
+            nn.Conv2d(512, 512, kernel_size=1, stride=1, padding=0),
+            CBAM(channels=512)
         )
-        self.layer_norm = nn.LayerNorm([1024, 64, 64])
-        self.layer_norm_p = nn.LayerNorm([1024, 16, 16])
-        self.position_embeddings = nn.Parameter(torch.randn(4, 4, 1024))
-
+        self.layer_norm = nn.LayerNorm([512, 64, 64])
+        self.layer_norm_p = nn.LayerNorm([512, 16, 16])
+        self.position_embeddings = nn.Parameter(torch.randn(4, 4, 512))
+        self.dyt = DyT()
     def forward(self, x):
         img_size = x.shape[-1]  # 256
         
@@ -289,7 +308,8 @@ class LoGoEncoder(nn.Module):
         x = self.conv1(x)       # 256x256 -> 64x64
         x = self.layer_norm(x)
         x = self.layer(x)
-        x_norm = self.layer_norm(x)
+        #x_norm = self.layer_norm(x)
+        x_norm = self.dyt(x)
         #LOCAL FROM THIS
         x_loc = x.clone()       # Shape: [1, 512, 64, 64]
         patch_size = img_size // 4  # 64
@@ -317,10 +337,11 @@ class LoGoEncoder(nn.Module):
                       output_size*j:output_size*(j+1)] = x_p_pos 
         #COMBINE LOCAL AND GLOBAL
 
-        x_loc_norm = self.layer_norm(x_loc)
+        #x_loc_norm = self.layer_norm(x_loc)
+        x_loc_norm = self.dyt(x_loc)
         x_combine = torch.add(x_norm, x_loc_norm)  ## Shape: [1, 512, 64, 64]
         x_combine = self.adjust_p(x_combine) #CBAM
-        x_combine = self.layer_norm(x_combine)
+        # x_combine = self.layer_norm(x_combine)
         out = self.relu(x_combine)
         
         return out
